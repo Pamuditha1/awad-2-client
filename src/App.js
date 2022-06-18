bedc53893266ec1b94c478542685ed5d8a4af3aa
Pamuditha Rajapaksha <pamuditharajapaksha@gmail.com>
2022-06-18 23:32:38 +0530

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { API } from "./api";
+import { API, CURR_KEY } from "./api";
 
 function App() {
   const [usdValue, setUsdValue] = useState("");
@@ -24,43 +24,64 @@ function App() {
       myHeaders.append("apikey", "9sjF2CMC9M0DV9F5kV0VNDHZMb2GJ4cV");
 
       const { data } = await axios.get(
-        "https://api.apilayer.com/currency_data/convert?to=lkr&from=usd&amount=1",
-        { headers: { apikey: "9sjF2CMC9M0DV9F5kV0VNDHZMb2GJ4cV" } }
+        "https://api.currencyapi.com/v3/latest?apikey=oEo4QuWCuGAgqfe3jquWPA5KxFVj93hHv1Fe0LP9&base_currency=USD&currencies=LKR"
       );
-
-      const lkrVal = +data.result.toFixed(2);
+      const lkrVal = +data.data.LKR.value.toFixed(2);
       setlkrValue(lkrVal);
 
-      const valueT = (+data.result.toFixed(2) * usdValue).toFixed(2);
+      const valueT = (+data.data.LKR.value.toFixed(2) * usdValue).toFixed(2);
       setvalue(valueT);
 
       const date = new Date().toLocaleDateString();
       const time = new Date().toLocaleTimeString();
 
-      await axios.post(`${API}/addCurrency`, { date, time, value: lkrVal });
+      // await axios.post(`${API}/addCurrency`, { date, time, value: lkrVal });
 
       getValues();
     }
   };
   return (
     <div className="App">
-      <p>USD</p>
-      <input
-        name="usd"
-        value={usdValue}
-        onChange={(e) => setUsdValue(e.target.value)}
-        onKeyPress={(e) => changeUSD(e)}
-      />
+      <div
+        style={{
+          backgroundColor: "lightblue",
+          borderRadius: 50,
+          boxShadow: "3px 3px 10px 3px grey",
+          margin: 400,
+          marginTop: 150,
+          padding: 50,
+          overflowY: "hidden",
+        }}
+      >
+        <div>
+          {" "}
+          <p>USD</p>
+          <input
+            name="usd"
+            value={usdValue}
+            onChange={(e) => setUsdValue(e.target.value)}
+            onKeyPress={(e) => changeUSD(e)}
+          />
+        </div>
 
-      <h4 style={{ color: "red" }}>Rate {lkrValue && ` - LKR ${lkrValue}`}</h4>
+        <h4 style={{ color: "blue" }}>
+          {usdValue} * {lkrValue && `${lkrValue}`}
+        </h4>
 
-      <div style={{ marginTop: 50 }}>
-        <h2>LKR</h2>
-        <h2>{value}</h2>
+        <div style={{ marginTop: 50 }}>
+          <h2>LKR</h2>
+          <h2>{value}</h2>
+        </div>
+        <div style={{ marginTop: 20 }}>
+          <h6>
+            {new Date().toLocaleDateString()} -{" "}
+            {new Date().toLocaleTimeString()}
+          </h6>
+        </div>
       </div>
 
-      <div style={{ marginTop: 100 }}>
-        {!valueList || valueList.length === 0 ? (
+      {/* <div style={{ marginTop: 100 }}>
+        {valueList?.length === 0 ? (
           <p>
             <b>
               <i>Loading...</i>
@@ -77,7 +98,7 @@ function App() {
             );
           })
         )}
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,1 +1,2 @@
-export const API = "http://localhost:8080/api/currency"
+export const API = "http://localhost:8080/api/currency";
+export const CURR_KEY = "9sjF2CMC9M0DV9F5kV0VNDHZMb2GJ4cV";
